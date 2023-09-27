649c111498915fb01df8e6c6dd268801df34fda9
VitorLuckmann <106416066+VitorLuckmann@users.noreply.github.com>
2023-09-27 20:45:21 -0300

    Update Atividade-1.php

--- AtividadePHP/Atividade-1.php
+++ AtividadePHP/Atividade-1.php
@@ -4,32 +4,36 @@
     <title>Soma de Três Valores</title>
 </head>
 <body>
+    <h1>Soma de Três Valores</h1>
+    
+    <form method="post">
+        Valor 1: <input type="text" name="valor1"><br>
+        Valor 2: <input type="text" name="valor2"><br>
+        Valor 3: <input type="text" name="valor3"><br>
+        <input type="submit" name="calcular" value="Calcular">
+    </form>
+
     <?php
-    // Defina os valores das três variáveis
-    $valor1 = 15; // Você pode alterar os valores conforme necessário
-    $valor2 = 8;
-    $valor3 = 12;
+    if(isset($_POST['calcular'])){
+        $valor1 = intval($_POST['valor1']);
+        $valor2 = intval($_POST['valor2']);
+        $valor3 = intval($_POST['valor3']);
 
-    // Calcule a soma dos três valores
-    $soma = $valor1 + $valor2 + $valor3;
+        $resultado = $valor1 + $valor2 + $valor3;
 
-    // Inicialize uma variável para armazenar a cor do texto
-    $corTexto = '';
-
-    // Verifique as condições e defina a cor do texto
-    if ($valor1 > 10) {
-        $corTexto = 'blue';
-    } elseif ($valor2 < $valor3) {
-        $corTexto = 'green';
-    } elseif ($valor3 < $valor1 && $valor3 < $valor2) {
-        $corTexto = 'red';
+        if($valor1 > 10){
+            echo '<p style="color: blue;">O resultado da soma é: ' . $resultado . '</p>';
+        }
+        elseif($valor2 < $valor3){
+            echo '<p style="color: green;">O resultado da soma é: ' . $resultado . '</p>';
+        }
+        elseif($valor3 < $valor1 && $valor3 < $valor2){
+            echo '<p style="color: red;">O resultado da soma é: ' . $resultado . '</p>';
+        }
+        else{
+            echo '<p>O resultado da soma é: ' . $resultado . '</p>';
+        }
     }
-
-    // Crie um estilo CSS inline com base na cor selecionada
-    $estiloTexto = 'color: ' . $corTexto . ';';
-
-    // Imprima o resultado com a cor apropriada
-    echo '<p style="' . $estiloTexto . '">A soma é: ' . $soma . '</p>';
     ?>
 </body>
 </html>
